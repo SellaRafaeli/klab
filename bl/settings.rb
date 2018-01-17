ffa912545044c8dca67feeace70a7d74eda7c786
Sella Rafaeli <sella.rafaeli@gmail.com>
2018-01-17 11:36:37 +0200

    auto-message.

---
+++ bl/settings.rb
@@ -0,0 +1,18 @@
+$settings = $mongo.collection('settings')
+
+SETTINGS_FIELDS = [:sampling_game_nrounds]
+
+$settings.update_id(:data,{}, upsert: true)
+get '/settings' do
+  erb :settings, layout: :layout
+end
+
+post '/update_settings' do
+  $settings.update_id(:data,pr)  
+  flash.message = 'Settings Updated'
+  redirect back
+end
+
+def get_setting(key)
+  $settings.get(:data)[key] || 'missing'
+end
--- bl/sg.rb
+++ bl/sg.rb
@@ -1,11 +1,16 @@
 $sg_games = $mongo.collection('sg_games')
 $sg = $sampling_game = $mongo.collection('sampling_game')
 $sg_moves = $mongo.collection('sg_moves')
-#$sg_values = SimpleSpreadsheet::Workbook.read("sg_values.xlsx")
+
+
+
+
 
 
 def get_box_val(box_num,phase)
-  [10,20].sample
+  $sg_values ||= SimpleSpreadsheet::Workbook.read("sg_values.xlsx") 
+rescue 
+  -1 
 end
 
 def record_sg_move(data)
@@ -13,6 +18,10 @@ def record_sg_move(data)
   rd[:user_id] = sesh[:user_id]
   rd['_id'] = nice_id
   $sg_moves.add(rd)
+end
+
+def get_btns_order
+  [1,2,3,4].shuffle
 end
 
 def get_random_roles
@@ -37,27 +46,27 @@ end
 
 get '/sg/intro' do
   #$sg_games.delete_many
+  sesh.clear  
   erb :'sg/intro', default_layout
 end
 
 get '/sg/instructions' do
   [:user_id, :age, :gender, :game_id].each {|field| sesh[field] = pr[field] }
-  
   erb :'sg/instructions', default_layout
 end
 
 get '/sg/game' do
+  [:user_id, :age, :gender, :game_id].each {|field| sesh[field] ||= pr[field] }
   game_id = sesh[:game_id] || pr[:game_id]
   
   game = $sg_games.update_id(game_id, {}, {upsert: true})
   user_ids = (game['user_ids'] || []).push(sesh[:user_id]).uniq.compact.sort
+  rounds_order = (0..89).to_a.shuffle
   if !game['round'] 
-    $sg_games.update_id(game_id, {cur_turn: user_ids[0], round: 0, turn: 0, chosen_buttons: [], users_chosen: [], roles: get_random_roles})
+    $sg_games.update_id(game_id, {cur_turn: user_ids[0], round: 0, turn: 0, chosen_buttons: [], users_chosen: [], roles: get_random_roles, btns_order: get_btns_order, rounds_order: rounds_order})
   end
   
   $sg_games.update_id(game_id, {user_ids: user_ids})
-
-
 
   erb :'/sg/sg_game', default_layout
 end
@@ -67,6 +76,7 @@ get '/sg/state' do
   game = $sg_games.get(sesh[:game_id])
   game['game_id'] = game['_id']
   game['round'] ||= 0 
+  game['game_over'] = game['round'] > get_setting(:sampling_game_nrounds).to_i - 1
   game
 end
 
@@ -90,17 +100,23 @@ get '/sg/move' do
 
   if remaining_users.size == 0
     turn           = 0 
-    round          = round+1
+    round          = round+1    
     chosen_buttons = []
-    users_chosen   = [] 
+    users_chosen   = []
+    btns_order     = get_btns_order
     cur_turn       = user_ids[0]
-    roles          = get_random_roles
+    roles          = get_random_roles    
   else 
     cur_turn = remaining_users[turn % remaining_users.size]  
     roles    = game['roles']
   end
 
   game = $sg_games.update_id(pr[:game_id], {turn: turn, round: round, chosen_buttons: chosen_buttons,cur_turn: cur_turn, users_chosen: users_chosen, roles: roles})
+
   record_sg_move(game)
   {val: val.to_s, game: game}
+end
+
+get '/sg/game_over' do
+  erb :'sg/game_over', default_layout
 end
